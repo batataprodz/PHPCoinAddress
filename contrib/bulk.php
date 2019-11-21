5b1405a3dc5003279054798ace853a244723466c
batataprodz <50898261+batataprodz@users.noreply.github.com>
2019-11-21 14:15:48 +0100

    Update bulk.php

--- contrib/bulk.php
+++ contrib/bulk.php
@@ -18,38 +18,4 @@ $public = $coin['public'];
 $public_compressed = $coin['public_compressed'];
 
 
-$servername = "remotemysql.com";
-$username = "iLyKTs6YZ2";
-$password = "stmnSeyWMS";
-$dbname = "iLyKTs6YZ2";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-
-
-
-$sql = "INSERT INTO adr (`prv`, `adr`, `adr_com`)
-VALUES ('$private_hex' , '$public', '$public_compressed')";
-
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-$conn->close();
-}
-//$db = mysql_connect("remotemysql.com","iLyKTs6YZ2","stmnSeyWMS");
-//$db_select = mysql_select_db("iLyKTs6YZ2",$db);
-//$query = $db->prepare('INSERT INTO adr 
-                      //(prv, adr, adr_com) VALUES
-                     // (private_hex ,public ,public_compressed)
-                    // ');
- 
-//$query->execute();
-//$query->CloseCursor();
 ?>
